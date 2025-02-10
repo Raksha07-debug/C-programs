420213bdea0a0290f29472efe51daf987579bc20
Raksha <rak070803@gmail.com>
2025-02-10 23:07:14 +0530

    Added:bmi of a person

---
+++ Phase1/BmiCalculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float height = 1.82;
+    float weight = 72;
+    float bmi = weight / (height * height);
+    
+//print bmi of an individual 
+     printf("%.2f\n", bmi));
+    
+    return 0;
+}
