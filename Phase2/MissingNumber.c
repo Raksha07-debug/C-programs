8970f6bc84cac78fa615e10adc294ce026e59c19
Raksha07-debug <rak070803@gmail.com>
2025-02-25 10:48:37 +0530

    Added:Print the missing number

--- Phase2/MissingNumber.c
+++ Phase2/MissingNumber.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//method to check missing number
+//method to check missing number.
 int missingNumber(int* nums, int numsSize) {
     int expectedSum = numsSize * (numsSize + 1) / 2;
     int actualSum = 0;
