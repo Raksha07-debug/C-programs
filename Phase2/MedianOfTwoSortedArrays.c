ccbc0047244e7d9d4d716700481b51bdfbde50a9
Raksha07-debug <rak070803@gmail.com>
2025-02-25 10:48:09 +0530

    Added:Median of two sorted arrays

--- Phase2/MedianOfTwoSortedArrays.c
+++ Phase2/MedianOfTwoSortedArrays.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    // This code is written by Anand Soni
+
     if (nums1Size > nums2Size) {
         return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
     }
